1caf3c40ae706d321aed2b2ae84c9797f15f8acb
drshnkini <alphacoresolutions@gmail.com>
2022-05-07 15:59:48 +0530

    seerver database connection

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
 
-$con = mysqli_connect("localhost", "root", "", "iitminds") or die ("<html><script language='JavaScript'>alert('Unable to connect to database! Please try again later.'),history.go(-1)</script></html>");
+$con = mysqli_connect("localhost", "u420187958_iitminds", "Drshn@1710", "u420187958_iitminds") or die ("<html><script language='JavaScript'>alert('Unable to connect to database! Please try again later.'),history.go(-1)</script></html>");
 
-$base_url = 'http://' . $_SERVER['HTTP_HOST'] . '/iit-minds.com/';
+$base_url = 'http://' . $_SERVER['HTTP_HOST'] . '/';
 
 $current_url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
-$image_url = 'http://' . $_SERVER['HTTP_HOST'] . '/iit-minds.com/cms/';
+$image_url = 'http://' . $_SERVER['HTTP_HOST'] . '/cms/';
 ?>
 
