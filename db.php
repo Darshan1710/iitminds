bf26acdb4e53865ad3e7fa7ebe6995890a75da9a
drshnkini <alphacoresolutions@gmail.com>
2022-05-07 16:03:02 +0530

    add https

--- db.php
+++ db.php
@@ -2,10 +2,10 @@
 
 $con = mysqli_connect("localhost", "u420187958_iitminds", "Drshn@1710", "u420187958_iitminds") or die ("<html><script language='JavaScript'>alert('Unable to connect to database! Please try again later.'),history.go(-1)</script></html>");
 
-$base_url = 'http://' . $_SERVER['HTTP_HOST'] . '/';
+$base_url = 'https://' . $_SERVER['HTTP_HOST'] . '/';
 
-$current_url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+$current_url = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
-$image_url = 'http://' . $_SERVER['HTTP_HOST'] . '/cms/';
+$image_url = 'https://' . $_SERVER['HTTP_HOST'] . '/cms/';
 ?>
 
